b9c658922134de6cb7a66301e74b6eb9d41e3447
Guilherme <p000gabreu@prservicos.com.br>
2019-08-17 19:27:51 -0300

    Adding improvements on Google API search

--- scrapers/google/scraper.py
+++ scrapers/google/scraper.py
@@ -6,8 +6,19 @@ CSE_ID = '013040238308795042536:ovfr8s2onym'
 
 
 def google_search(search_term, api_key, cse_id, **kwargs):
-    service = build("customsearch", "v1", developerKey=API_KEY)
+    service = build("customsearch", "v1", developerKey=api_key)
     return service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
 
-result = google_search("Guilherme+Abreu", API_KEY, CSE_ID)
-print(result)
+
+def get_itens(result):
+    return result['items']
+
+def get_good_itens(items):
+    for item in items:
+        if item['displayLink'] == 'br.linkedin.com' or item['displayLink'] == 'pt-br.facebook.com':
+            print(item['formattedUrl'])
+
+
+result = google_search("Marcel Rapanelli", API_KEY, CSE_ID)
+items = get_itens(result)
+get_good_itens(items)
