e924dfaa8daf179101583e8e755866f4ffcf4884
Guilherme <p000gabreu@prservicos.com.br>
2019-08-17 19:36:43 -0300

    Cleaning the code

--- scrapers/google/scraper.py
+++ scrapers/google/scraper.py
@@ -1,11 +1,13 @@
 from googleapiclient.discovery import build
 
-SEARCH_URL = 'https://www.google.com/search?q='
 API_KEY = 'AIzaSyC2uE6WMCPO2xcTsqULI_wa_iwPhP5Zzj0'
 CSE_ID = '013040238308795042536:ovfr8s2onym'
 
 
 def google_search(search_term, api_key, cse_id, **kwargs):
+    filter_itens(get_itens(get_result(search_term, api_key, cse_id, **kwargs)))
+
+def get_result(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
     return service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
 
@@ -13,12 +15,11 @@ def google_search(search_term, api_key, cse_id, **kwargs):
 def get_itens(result):
     return result['items']
 
-def get_good_itens(items):
+def filter_itens(items):
     for item in items:
         if item['displayLink'] == 'br.linkedin.com' or item['displayLink'] == 'pt-br.facebook.com':
             print(item['formattedUrl'])
 
 
-result = google_search("Marcel Rapanelli", API_KEY, CSE_ID)
-items = get_itens(result)
-get_good_itens(items)
+google_search("Marcel Rapanelli", API_KEY, CSE_ID)
+
